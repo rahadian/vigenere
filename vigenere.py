dc77affa31feaebb54435d194580e4d4ab572e26
AryaTux <rahadian271094@gmail.com>
2018-12-29 21:24:23 +0700

    fix the alphabet with value >= 26

--- vigenere.py
+++ vigenere.py
@@ -30,14 +30,14 @@ print "Arithmetic Substitution"
 for (z,k) in zip(input,itertools.cycle(key)):
     hasil = alpatoarray.index(z)+alpatoarray.index(k)
     # hasilarray = hasil.split("\r")
-    if hasil > 26:
+    if hasil >= 26:
         hasil = alpatoarray.index(z)+alpatoarray.index(k)-26
     print z,'=',alpatoarray.index(z),"+",k,'=',alpatoarray.index(k),'=',hasil
 
 print "\r"
 for (m,n) in zip(input,itertools.cycle(key)):
     hasil = alpatoarray.index(m)+alpatoarray.index(n)
-    if hasil > 26:
+    if hasil >= 26:
         hasil = alpatoarray.index(m)+alpatoarray.index(n)-26
     hasilarray = np.asarray(hasil)
     print hasilarray,'=',alpatoarray[hasilarray]
@@ -46,7 +46,7 @@ print "Chipertext ",
 print "\r"
 for (m,n) in zip(input,itertools.cycle(key)):
     hasil = alpatoarray.index(m)+alpatoarray.index(n)
-    if hasil > 26:
+    if hasil >= 26:
         hasil = alpatoarray.index(m)+alpatoarray.index(n)-26
     hasilarray = np.asarray(hasil)
     strhasil = str(alpatoarray[hasilarray])
